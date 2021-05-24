34f861a6fc5b6ec4cfae2755093cbd2a9a098639
MehaletKB <mkesatebirhan@gmail.com>
2021-05-24 15:57:12 -0500

    file refactor for better legibility

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-// Assignment Code
+// Assign variable to generate id
 var generateBtn = document.querySelector("#generate");
+
+
+// Assignment characters to variables
 var numbersChar = ["1","2","3","4","5","6","7","8","9","0"];
 var uppercaseChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowercaseChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var symbolChar = [",","!","#","$","%","&","'","*","+","-",".","/",":",";","<",">","=","?","@"];
 
-var includeNumbers = false;
-var includeUppercase = false;
-var includeLowercase = false;
-var includeSymbol = false;
 
-
-
+// Insert template string of options into HTML 
 function includeOptions() {
-  var formHtml = 
-  `<input type='checkbox' id="include-numbers">
+  var optionsHTML = 
+  `<input type="checkbox" id="include-numbers">
   <label>Include Numbers</label>
-  <input type='checkbox' id="include-uppercase">
+  <input type="checkbox" id="include-uppercase">
   <label>Include Uppercase</label>
-  <input type='checkbox' id="include-lowercase">
+  <input type="checkbox" id="include-lowercase">
   <label>Include Lowercase</label>
-  <input type='checkbox' id="include-symbol">
+  <input type="checkbox" id="include-symbol">
   <label>Include Symbols</label>`
   
   var options = document.querySelector(".card-footer");
-  options.insertAdjacentHTML('beforebegin', formHtml);
+  options.insertAdjacentHTML('beforebegin', optionsHTML);
 }
 includeOptions();
 
 
 function generatePassword(){
+  // Set empty arrays to push values into
   var possibleChar = [];
   var randomIndex = [];
   
+  // Assign state to variables assigned to ids
   var numberCheckbox = document.querySelector("#include-numbers");
   includeNumbers = numberCheckbox.checked;
   
@@ -44,47 +44,46 @@ function generatePassword(){
   
   var symbolCheckbox = document.querySelector ("#include-symbol")
   includeSymbol = symbolCheckbox.checked;
-
+  
+  // Push values of chosen array/from checked boxes into array of possible characters
   if(includeNumbers){
     possibleChar.push(...numbersChar);
   }
-
   if(includeUppercase){
     possibleChar.push(...uppercaseChar);
   }
-
   if(includeLowercase){
     possibleChar.push(...lowercaseChar);
   }
-  
   if(includeSymbol){
     possibleChar.push(...symbolChar);
   }
-
+  
+  // Set user input parameters, determine password length based on user input
   if(includeNumbers || includeUppercase || includeLowercase || includeSymbol){
     var howMany = parseInt(prompt("How many characters would you like?"));
   }else{
-    alert("Please select at least one of the options below.");
-    return;
+    alert("Please select one or more of the option(s) below.");
+    return (null);
   }
-  
   if (howMany < 8 || howMany > 128 || isNaN(howMany)){
     alert("Please enter a number between 8 and 128");
-    return;
   }else{
     var passwordLength = howMany;
   }
-
+  
+  // Randomly select characters from possible characters array based on user input
   for(i = 0; i < passwordLength; i++){
-    var getChar = possibleChar[Math.floor(Math.random() * passwordLength)] ;
+    var getChar = possibleChar[Math.floor(Math.random() * possibleChar.length)] ;
     randomIndex.push(getChar);
   }
-   console.log(randomIndex)
+  
+  // console.log(randomIndex);
   return randomIndex.join("");
 }
 
 
-
+// Set password text based on generatePassword function
 function writePassword() {
   var passwordText = document.querySelector("#password");
   var password = generatePassword();
